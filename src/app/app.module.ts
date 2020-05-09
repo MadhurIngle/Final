1beccbed2f69de70fa3f369665b87a02aba2fc99
Madhur <inglemadhur57@gmail.com>
2020-05-09 22:51:30 +0530

    step 1 new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EvenNumberComponent } from './even-number/even-number.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EvenNumberComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/even-number/even-number.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-even-number',
+  templateUrl: './even-number.component.html',
+  styleUrls: ['./even-number.component.scss']
+})
+export class EvenNumberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
